3c871c5cc50700b07429871a9fbfe91594e4c775
sem1hy1lmaz <yilmazsemih025@gmail.com>
2025-01-17 14:50:22 +0300

    Proje 3 ve Proje 4
    ADCden değer okuma ve SAAT başlangıcı gönderme

---
+++ ADC ve SERİPORT PROJESİ/Debug/seriportlcd.d
@@ -0,0 +1,69 @@
+# FIXED
+
+seriportlcd.obj: ../seriportlcd.c
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+seriportlcd.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+seriportlcd.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h
+seriportlcd.obj: ../seriportlcd.h
+
+../seriportlcd.c:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h:
+
+../seriportlcd.h:
+
